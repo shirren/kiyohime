8cece1a9e0eab3ac8530e815b0b2ae19acf67a26
Shirren Premaratne <shirren@me.com>
2016-05-06 15:07:52 +1000

    Ran rubocop

--- lib/kiyohime/exceptions/registry_error.rb
+++ lib/kiyohime/exceptions/registry_error.rb
@@ -2,7 +2,6 @@ module Kiyohime
   module Exceptions
     # This error is used to signal when the registry has an issue
     class RegistryError < StandardError
-
       attr_reader :message
 
       def initialize(message)
--- lib/kiyohime/exceptions/subscriber_error.rb
+++ lib/kiyohime/exceptions/subscriber_error.rb
@@ -2,7 +2,6 @@ module Kiyohime
   module Exceptions
     # This error is used to signal when a subscriber throws an error
     class SubscriberError < StandardError
-
       attr_reader :message, :action, :data
 
       def initialize(message, action = nil, data = nil)
